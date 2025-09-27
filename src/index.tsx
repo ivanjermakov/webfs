dfa7390dd25de130c6759a4fead68857e4f95d44
ivanjermakov <ivanjermakov1@gmail.com>
2025-09-27 16:57:14 +0200

    Header

--- src/index.tsx
+++ src/index.tsx
@@ -3,32 +3,41 @@ import { render } from 'solid-js/web'
 import './index.css'
 import { Component, createSignal, For, Match, onMount, Switch } from 'solid-js'
 
+type File = {
+    path: string
+    content: string
+}
+
 const Main: Component = () => {
     const [files, setFiles] = createSignal<string[] | undefined>()
-    const [content, setContent] = createSignal<string | undefined>()
+    const [file, setFile] = createSignal<File | undefined>()
     onMount(async () => {
-        const path = location.pathname
-        if (path === '/' || path === '/index.html') {
+        const pathname = location.pathname
+        if (pathname === '/' || pathname === '/index.html') {
             const files_ = await (await fetch('/api/files')).json()
             setFiles(files_)
             console.log(files_)
         } else {
-            const params = new URLSearchParams({ path: path.slice(1) });
+            const path = pathname.slice(1)
+            const params = new URLSearchParams({ path });
             const content_ = await (await fetch(`/api/file?${params}`, {})).text()
-            setContent(content_)
-            console.log(content_)
+            setFile({ path, content: content_ })
+            console.log(file())
         }
     })
-    return <Switch>
-        <Match when={content() !== undefined}>
-            <textarea>{content()}</textarea>
-        </Match>
-        <Match when={files() !== undefined}>
-            <div class="directory">
-                <For each={files()}>{file => <a href={`/${file}`}>{file}</a>}</For>
-            </div>
-        </Match>
-    </Switch>
+    return <>
+        <div class="header">{file()?.path ?? 'directory'}</div>
+        <Switch>
+            <Match when={file() !== undefined}>
+                <textarea>{file()!.content}</textarea>
+            </Match>
+            <Match when={files() !== undefined}>
+                <div class="directory">
+                    <For each={files()}>{file => <a href={`/${file}`}>{file}</a>}</For>
+                </div>
+            </Match>
+        </Switch>
+    </>
 }
 
 render(() => <Main />, document.getElementById('root')!)
